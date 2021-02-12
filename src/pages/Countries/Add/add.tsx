566ed15ad2dccd500a984084c94a6ec237219d88
Benjamin Domey <domeybenjamin1@gmail.com>
2021-02-12 15:30:14 +0000

    u can now save to db

--- src/pages/Countries/Add/add.tsx
+++ src/pages/Countries/Add/add.tsx
@@ -1,16 +1,55 @@
+import { ApolloError, useMutation } from "@apollo/client";
+import { toaster } from "evergreen-ui";
 import * as React from "react";
 import { BasicModal } from "../../../components/atoms/modal";
-
+import { CREATE_COUNTRY } from "../../../services/graphql/mutations";
+import {
+  CreateCountryInputProps,
+  CreateCountryOutputProps,
+} from "../../../shared/interfaces/country";
+import _ from "lodash";
 interface Props {
   show: boolean;
   setShow: React.Dispatch<React.SetStateAction<boolean>>;
   refetch: any;
 }
 
-const AddCountry: React.FC<Props> = ({ setShow, show }) => {
+const AddCountry: React.FC<Props> = ({ setShow, show, refetch }) => {
   const [name, setName] = React.useState<string>("");
   const [currency, setCurrency] = React.useState<string>("");
   const [description, setDescription] = React.useState<string>("");
+
+  const [addInvoker, { loading }] = useMutation<
+    CreateCountryOutputProps,
+    CreateCountryInputProps
+  >(CREATE_COUNTRY);
+
+  const HandleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    addInvoker({
+      variables: {
+        name: name.trim(),
+        currency: currency || undefined,
+        description: description || undefined,
+      },
+    })
+      .then(() => {
+        refetch();
+        toaster.success("Added " + name + " successfully");
+        setShow(false);
+        setName("");
+        setDescription("");
+        setCurrency("");
+      })
+      .catch((e: ApolloError) => {
+        if (e?.graphQLErrors?.length > 0) {
+          return toaster.warning(
+            _.startCase(_.camelCase(e?.graphQLErrors[0]?.message))
+          );
+        }
+      });
+  };
+
   return (
     <React.Fragment>
       <BasicModal show={show} setShow={setShow}>
@@ -40,7 +79,7 @@ const AddCountry: React.FC<Props> = ({ setShow, show }) => {
 
           <div className="mt-2 p-5">
             <span className={"font-bold"}>Add New Country</span>
-            <form className={"mt-3"}>
+            <form onSubmit={HandleSubmit} className={"mt-3"}>
               <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-6">
                 <div className="sm:col-span-6">
                   <label
@@ -90,7 +129,6 @@ const AddCountry: React.FC<Props> = ({ setShow, show }) => {
                   </label>
                   <div className="mt-1 rounded-none shadow-sm">
                     <textarea
-                      required
                       value={description}
                       rows={5}
                       onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -105,6 +143,7 @@ const AddCountry: React.FC<Props> = ({ setShow, show }) => {
               <div className="pt-2 border-t border-gray-200 mt-5 flex justify-end">
                 <span className="inline-flex rounded-none shadow-sm mr-2 ">
                   <button
+                    disabled={loading}
                     type="button"
                     onClick={() => setShow(false)}
                     className="inline-flex rounded-none items-center px-6 py-2 border border-red-500 text-sm leading-5 font-light text-red-500 hover:text-white bg-white hover:bg-red-400 focus:outline-none focus:shadow-outline-blue focus:border-red-600 active:bg-blue-600 transition duration-150 ease-in-out"
@@ -114,10 +153,11 @@ const AddCountry: React.FC<Props> = ({ setShow, show }) => {
                 </span>
                 <span className="inline-flex rounded-none shadow-sm ">
                   <button
-                    type="button"
+                    disabled={loading}
+                    type="submit"
                     className="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-light rounded-none text-white bg-red-500 hover:bg-red-400 focus:outline-none focus:shadow-outline-teal focus:border-red-600 active:bg-blue-600 transition duration-150 ease-in-out"
                   >
-                    Add Country
+                    {loading ? "Adding..." : "Add Country"}
                   </button>
                 </span>
               </div>
