375b4e2712953898cc04ad215b6e1a66cca0b29d
Benjamin Domey <domeybenjamin1@gmail.com>
2021-02-12 15:30:40 +0000

    you can now delete country

--- src/pages/Countries/Delete/main.tsx
+++ src/pages/Countries/Delete/main.tsx
@@ -1,15 +1,51 @@
+import { useMutation, ApolloError } from "@apollo/client";
 import * as React from "react";
 import { BasicModal } from "../../../components/atoms/modal";
 import DeleteAsset from "../../../components/atoms/svgs/delete";
+import { DELETE_COUNTRY } from "../../../services/graphql/mutations";
+import {
+  Country,
+  DeleteCountryInputProps,
+  DeleteCountryOutputProps,
+} from "../../../shared/interfaces/country";
+import { toaster } from "evergreen-ui";
+import _ from "lodash";
 
 interface Props {
   show: boolean;
   setShow: React.Dispatch<React.SetStateAction<boolean>>;
   refetch: any;
-  data: any;
+  data: Country;
 }
 
-const DeleteCountry: React.FC<Props> = ({ setShow, show, data }) => {
+const DeleteCountry: React.FC<Props> = ({ setShow, show, data, refetch }) => {
+  const [deleteInvoker, { loading }] = useMutation<
+    DeleteCountryOutputProps,
+    DeleteCountryInputProps
+  >(DELETE_COUNTRY);
+
+  const HandleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    deleteInvoker({
+      variables: {
+        id: data?.id,
+      },
+    })
+      .then(() => {
+        refetch();
+        toaster.success(data?.name + " has been deleted successfully");
+        setShow(false);
+      })
+      .catch((e: ApolloError) => {
+        if (e?.graphQLErrors?.length > 0) {
+          return toaster.warning(
+            _.startCase(_.camelCase(e?.graphQLErrors[0]?.message))
+          );
+        }
+      });
+  };
+
   return (
     <React.Fragment>
       <BasicModal show={show} setShow={setShow}>
@@ -39,7 +75,7 @@ const DeleteCountry: React.FC<Props> = ({ setShow, show, data }) => {
 
           <div className="mt-2 p-5">
             <span className={"font-bold"}>Delete Country</span>
-            <form className={"mt-3"}>
+            <form onSubmit={HandleSubmit} className={"mt-3"}>
               <div className="flex items-center flex-col">
                 <DeleteAsset className={" h-40 w-40"} />
                 <span>Are you sure you want to delete `{data?.name}`</span>
@@ -47,6 +83,7 @@ const DeleteCountry: React.FC<Props> = ({ setShow, show, data }) => {
               <div className="pt-2 border-t border-gray-200 mt-5 flex justify-end">
                 <span className="inline-flex rounded-none shadow-sm mr-2 ">
                   <button
+                    disabled={loading}
                     type="button"
                     onClick={() => setShow(false)}
                     className="inline-flex rounded-none items-center px-6 py-2 border border-red-500 text-sm leading-5 font-light text-red-500 hover:text-white bg-white hover:bg-red-400 focus:outline-none focus:shadow-outline-blue focus:border-red-600 active:bg-blue-600 transition duration-150 ease-in-out"
@@ -56,10 +93,11 @@ const DeleteCountry: React.FC<Props> = ({ setShow, show, data }) => {
                 </span>
                 <span className="inline-flex rounded-none shadow-sm ">
                   <button
-                    type="button"
+                    disabled={loading}
+                    type="submit"
                     className="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-light rounded-none text-white bg-red-500 hover:bg-red-400 focus:outline-none focus:shadow-outline-teal focus:border-red-600 active:bg-blue-600 transition duration-150 ease-in-out"
                   >
-                    Yes, delete
+                    {loading ? " Deleting..." : "Yes, delete"}
                   </button>
                 </span>
               </div>
