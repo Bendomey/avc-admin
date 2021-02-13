b7f6b6277611b4781bd8761181c8544dcd2246a0
Benjamin Domey <domeybenjamin1@gmail.com>
2021-02-13 21:28:52 +0000

    typings for overrall user

---
+++ src/shared/interfaces/shared.ts
@@ -0,0 +1,12 @@
+export interface User {
+  id: string;
+  lastName: string;
+  otherNames: string;
+  firstName: string;
+  email: string;
+  phone: string;
+  setupAt: Date;
+  suspendedAt: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
