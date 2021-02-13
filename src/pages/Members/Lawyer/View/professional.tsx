2fa090aeb89c20f38ee0bd9ef289eda33191d9b3
Benjamin Domey <domeybenjamin1@gmail.com>
2021-02-13 21:29:39 +0000

    removed warnings

--- src/pages/Members/Lawyer/View/professional.tsx
+++ src/pages/Members/Lawyer/View/professional.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Lawyer } from "../../../../shared/interfaces/lawyer";
-import { format } from "date-fns";
+// import { format } from "date-fns";
 
 interface Props {
   data: Lawyer;
