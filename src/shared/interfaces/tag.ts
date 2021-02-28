81c57ad0d9cc505eadfc208690dba2580389e168
Benjamin Domey <domeybenjamin1@gmail.com>
2021-02-28 01:55:30 +0000

    added typings for altering tags in backend

--- src/shared/interfaces/tag.ts
+++ src/shared/interfaces/tag.ts
@@ -18,3 +18,30 @@ export interface Tag {
   createdAt: Date;
   updatedAt: Date;
 }
+
+export interface CreateTagInputProps {
+  name: string;
+}
+
+export interface CreateTagOutputProps {
+  createTag: {
+    id: string;
+  };
+}
+
+export interface UpdateTagInputProps {
+  id: string;
+  name?: string;
+}
+
+export interface UpdateTagOutputProps {
+  updateTag: boolean;
+}
+
+export interface DeleteTagInputProps {
+  id: string;
+}
+
+export interface DeleteTagOutputProps {
+  deleteTag: boolean;
+}
