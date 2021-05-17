0862f7eca04c0ceb3e79dce74586ca69d90d94f2
Benjamin Domey <domeybenjamin1@gmail.com>
2021-05-17 18:13:40 +0000

    added packages link and updated it

--- src/shared/layout/side.tsx
+++ src/shared/layout/side.tsx
@@ -502,6 +502,30 @@ const SideNav = () => {
                   </svg>
                   Jobs
                 </Link>
+                <Link
+                  to="/packages"
+                  className={`${
+                    pathname === "/packages"
+                      ? "bg-gray-200"
+                      : "hover:text-gray-900 hover:bg-gray-50"
+                  } text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md`}
+                >
+                  <svg
+                    className="text-gray-400 group-hover:text-gray-500 mr-3 h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
+                    />
+                  </svg>
+                  Packages
+                </Link>
               </div>
               <div className="mt-8">
                 <h3
