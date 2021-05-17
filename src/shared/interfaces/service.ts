560b3d679c3971e57afdabe53a3208f78547245d
Benjamin Domey <domeybenjamin1@gmail.com>
2021-05-17 18:11:11 +0000

    typings for services

---
+++ src/shared/interfaces/service.ts
@@ -0,0 +1,54 @@
+export interface Service {
+  id: string;
+  name: string;
+  price: number;
+  type: string;
+  description: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface GetServicesInputProps {
+  filter?: {};
+  skip?: number;
+  limit?: number;
+}
+
+export interface GetServicesOutputProps {
+  services: Service[];
+  servicesLength: number;
+}
+
+// for adding
+// export interface CreateServiceInputProps {
+//   name: string;
+//   currency?: string;
+//   description?: string;
+//   image?: string;
+// }
+
+// export interface CreateServiceOutputProps {
+//   createService: boolean;
+// }
+
+// for updating
+export interface UpdateServiceInputProps {
+  id: string;
+  name?: string;
+  price?: number;
+  description?: string;
+  type?: string;
+}
+
+export interface UpdateServiceOutputProps {
+  updateService: boolean;
+}
+
+// for deleting
+// export interface DeleteServiceInputProps {
+//   id: string;
+// }
+
+// export interface DeleteServiceOutputProps {
+//   deleteService: boolean;
+// }
