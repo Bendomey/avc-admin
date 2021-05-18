2e6d6f0e885661ec4782d409cbbd871cb57494d7
Benjamin Domey <domeybenjamin1@gmail.com>
2021-05-18 10:08:01 +0000

    made sure data is from db

--- src/pages/Packages/MainPackages/Card/card.tsx
+++ src/pages/Packages/MainPackages/Card/card.tsx
@@ -1,46 +1,44 @@
 import * as React from "react";
+import { Package } from "../../../../shared/interfaces/package";
 
 interface Props {
-  data: any;
+  data: Package;
+  period: "monthly" | "yearly";
 }
 
-const PackageCard: React.FC<Props> = ({ data }) => {
+const PackageCard: React.FC<Props> = ({ data, period }) => {
   return (
     <React.Fragment>
-      <div
-        key={data.name}
-        className="border border-gray-200 rounded-lg shadow-sm divide-y divide-gray-200"
-      >
+      <div className="border border-gray-200 rounded-lg shadow-sm divide-y divide-gray-200">
         <div className="p-6">
           <h2 className="text-lg leading-6 font-medium text-gray-900">
-            {data.name}
+            {data?.name}
           </h2>
           <p className="mt-4 text-sm text-gray-500">{data.description}</p>
           <p className="mt-8">
             <span className="text-4xl font-extrabold text-gray-900">
-              ${data.priceMonthly}
+              $
+              {period === "monthly"
+                ? data?.amountPerMonth
+                : data?.amountPerYear}
             </span>{" "}
-            <span className="text-base font-medium text-gray-500">/mo</span>
+            <span className="text-base font-medium text-gray-500">
+              /{period === "monthly" ? "mon" : "year"}
+            </span>
           </p>
-          <a
-            href={data.href}
-            className="mt-8 block w-full bg-gray-800 border border-gray-800 rounded-md py-2 text-sm font-semibold text-white text-center hover:bg-gray-900"
-          >
-            Edit {data.name}
-          </a>
-          <a
-            href={data.href}
-            className="mt-2 block w-full bg-red-800 border border-red-800 rounded-md py-2 text-sm font-semibold text-white text-center hover:bg-red-900"
-          >
-            Delete {data.name}
-          </a>
+          <button className="mt-8 block w-full bg-gray-800 border border-gray-800 rounded-md py-2 text-sm font-semibold text-white text-center hover:bg-gray-900">
+            Edit {data?.name}
+          </button>
+          <button className="mt-2 block w-full bg-red-800 border border-red-800 rounded-md py-2 text-sm font-semibold text-white text-center hover:bg-red-900">
+            Delete {data?.name}
+          </button>
         </div>
         <div className="pt-6 pb-8 px-6">
           <h3 className="text-xs font-medium text-gray-900 tracking-wide uppercase">
             What's included
           </h3>
           <ul className="mt-6 space-y-4">
-            {data.includedFeatures.map((feature: any) => (
+            {/* {data.includedFeatures.map((feature: any) => (
               <li key={feature} className="flex space-x-3">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -58,7 +56,7 @@ const PackageCard: React.FC<Props> = ({ data }) => {
                 </svg>
                 <span className="text-sm text-gray-500">{feature}</span>
               </li>
-            ))}
+            ))} */}
           </ul>
         </div>
       </div>
--- src/pages/Packages/RequestedPackage/Card/card.tsx
+++ src/pages/Packages/RequestedPackage/Card/card.tsx
@@ -1,51 +1,53 @@
 import * as React from "react";
+import { Package } from "../../../../shared/interfaces/package";
 
 interface Props {
-  data: any;
+  data: Package;
 }
 
 const PackageCard: React.FC<Props> = ({ data }) => {
+  const handleSubmit = (e: any) => {
+    e?.preventDefault();
+  };
   return (
     <React.Fragment>
-      <div
-        key={data.name}
-        className="border border-gray-200 rounded-lg shadow-sm divide-y divide-gray-200"
-      >
+      <div className="border border-gray-200 rounded-lg shadow-sm divide-y divide-gray-200">
         <div className="p-6">
           <h2 className="text-lg leading-6 font-medium text-gray-900">
-            {data.name}
+            {data?.name}
           </h2>
-          <p className="mt-4 text-sm text-gray-500">{data.description}</p>
-          <div className={"mt-3"}>
-            <input
-              type="number"
-              step={0.01}
-              className="shadow-sm focus:ring-red-500 bg-gray-50 focus:border-red-500 block w-full sm:text-sm border-gray-200 rounded-none"
-              placeholder="Price per month here - required"
-            />
-          </div>
-          <div className={"mt-3"}>
-            <input
-              type="number"
-              step={0.01}
-              className="shadow-sm focus:ring-red-500 bg-gray-50 focus:border-red-500 block w-full sm:text-sm border-gray-200 rounded-none"
-              placeholder="Price per year here - required"
-            />
-          </div>
+          <p className="mt-4 text-sm text-gray-500">{data?.description}</p>
+          <form onSubmit={handleSubmit}>
+            <div className={"mt-3"}>
+              <input
+                type="number"
+                step={0.01}
+                required
+                className="shadow-sm focus:ring-red-500 bg-gray-50 focus:border-red-500 block w-full sm:text-sm border-gray-200 rounded-none"
+                placeholder="Price per month here - required"
+              />
+            </div>
+            <div className={"mt-3"}>
+              <input
+                type="number"
+                required
+                step={0.01}
+                className="shadow-sm focus:ring-red-500 bg-gray-50 focus:border-red-500 block w-full sm:text-sm border-gray-200 rounded-none"
+                placeholder="Price per year here - required"
+              />
+            </div>
 
-          <a
-            href={data.href}
-            className="mt-8 block w-full bg-red-600 border border-red-800 rounded-md py-2 text-sm font-semibold text-white text-center hover:bg-red-900"
-          >
-            Update {data.name}
-          </a>
+            <button className="mt-8 block w-full bg-red-600 border border-red-800 rounded-md py-2 text-sm font-semibold text-white text-center hover:bg-red-900">
+              Update {data.name}
+            </button>
+          </form>
         </div>
         <div className="pt-6 px-6">
           <h3 className="text-xs font-medium text-gray-900 tracking-wide uppercase">
             What's included
           </h3>
           <ul className="mt-6  pb-4 space-y-4">
-            {data.includedFeatures.map((feature: any) => (
+            {/* {data.includedFeatures.map((feature: any) => (
               <li key={feature} className="flex space-x-3">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -63,11 +65,13 @@ const PackageCard: React.FC<Props> = ({ data }) => {
                 </svg>
                 <span className="text-sm text-gray-500">{feature}</span>
               </li>
-            ))}
+            ))} */}
           </ul>
           <h3 className="text-xs pb-3 font-medium mt-2 text-gray-900 tracking-wide">
             Requested By:{" "}
-            <span className={"text-gray-500"}>DOMEY BENJAMIN</span>
+            <span className={"text-gray-500"}>
+              {data?.requestedBy?.lastName} {data?.requestedBy?.firstName}
+            </span>
           </h3>
         </div>
       </div>
