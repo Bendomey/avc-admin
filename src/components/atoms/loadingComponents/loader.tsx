87e462aa94162628805c0bbd460f1ddcf5c731b8
Benjamin Domey <domeybenjamin1@gmail.com>
2021-05-18 10:06:50 +0000

    registered base loader

---
+++ src/components/atoms/loadingComponents/loader.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import PuffLoader from "react-spinners/PuffLoader";
+
+const CenterLoaders = () => {
+  return (
+    <React.Fragment>
+      <PuffLoader color={"#E21E47"} size={50} />
+    </React.Fragment>
+  );
+};
+
+export default CenterLoaders;
