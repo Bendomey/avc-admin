4a498328376d18b6debd44dd685c67c2647c2f1e
Benjamin Domey <domeybenjamin1@gmail.com>
2021-05-24 21:22:46 +0000

    feat(requested-package): fetch package services

--- src/pages/Packages/RequestedPackage/Card/card.tsx
+++ src/pages/Packages/RequestedPackage/Card/card.tsx
@@ -1,11 +1,30 @@
 import * as React from "react";
-import { Package } from "../../../../shared/interfaces/package";
+import { GET_PACKAGE_SERVICES } from "../../../../services/graphql/queries";
+import {
+  GetPackageServicesInputProps,
+  GetPackageServicesOutputProps,
+  IPackageService,
+  Package,
+} from "../../../../shared/interfaces/package";
+import { useQuery } from "@apollo/client";
+import { Loader } from "../../../../components/atoms/loadingComponents";
 
 interface Props {
   data: Package;
 }
 
 const PackageCard: React.FC<Props> = ({ data }) => {
+  const { data: packageServices, loading } = useQuery<
+    GetPackageServicesOutputProps,
+    GetPackageServicesInputProps
+  >(GET_PACKAGE_SERVICES, {
+    variables: {
+      filter: {
+        package: data?.id,
+      },
+    },
+  });
+
   const handleSubmit = (e: any) => {
     e?.preventDefault();
   };
@@ -42,38 +61,60 @@ const PackageCard: React.FC<Props> = ({ data }) => {
             </button>
           </form>
         </div>
-        <div className="pt-6 px-6">
-          <h3 className="text-xs font-medium text-gray-900 tracking-wide uppercase">
-            What's included
-          </h3>
-          <ul className="mt-6  pb-4 space-y-4">
-            {/* {data.includedFeatures.map((feature: any) => (
-              <li key={feature} className="flex space-x-3">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="flex-shrink-0 h-5 w-5 text-green-500"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M5 13l4 4L19 7"
-                  />
-                </svg>
-                <span className="text-sm text-gray-500">{feature}</span>
-              </li>
-            ))} */}
-          </ul>
-          <h3 className="text-xs pb-3 font-medium mt-2 text-gray-900 tracking-wide">
-            Requested By:{" "}
-            <span className={"text-gray-500"}>
-              {data?.requestedBy?.lastName} {data?.requestedBy?.firstName}
-            </span>
-          </h3>
-        </div>
+        {loading ? (
+          <React.Fragment>
+            <div className={"py-5 flex justify-center"}>
+              <Loader />
+            </div>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <div className="pt-6 pb-8 px-6">
+              <h3 className="text-xs font-medium text-gray-900 tracking-wide uppercase">
+                What's included
+              </h3>
+              <ul className="mt-6 space-y-4">
+                {packageServices?.packageServices?.map(
+                  (packageService: IPackageService, i: number) => (
+                    <li key={i} className="flex space-x-3">
+                      {packageService?.type === "BOOLEAN" ? (
+                        <React.Fragment>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="flex-shrink-0 h-5 w-5 text-green-500"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M5 13l4 4L19 7"
+                            />
+                          </svg>
+                        </React.Fragment>
+                      ) : (
+                        <React.Fragment>
+                          <span>{packageService?.quantity || 0}</span>
+                        </React.Fragment>
+                      )}
+                      <span className="text-sm text-gray-500">
+                        {packageService?.service?.name}
+                      </span>
+                    </li>
+                  )
+                )}
+              </ul>
+              <h3 className="text-xs pb-3 font-medium mt-2 text-gray-900 tracking-wide">
+                Requested By:{" "}
+                <span className={"text-gray-500"}>
+                  {data?.requestedBy?.lastName} {data?.requestedBy?.firstName}
+                </span>
+              </h3>
+            </div>
+          </React.Fragment>
+        )}
       </div>
     </React.Fragment>
   );
